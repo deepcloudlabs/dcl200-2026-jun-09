d2dcad89c92828c06c240a7acbca76260c190941
Binnur KURT <deepcloudlabs@gmail.com>
2026-06-11 15:01:14 +0300

    initial commit

--- benchmarking-collections/target/generated-sources/annotations/com/example/benchmarks/jmh_generated/ListBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.benchmarks.jmh_generated;
-public class ListBenchmark_jmhType extends ListBenchmark_jmhType_B3 {
-}
-
--- benchmarking-collections/target/generated-sources/annotations/com/example/benchmarks/jmh_generated/SetBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.benchmarks.jmh_generated;
-public class SetBenchmark_jmhType extends SetBenchmark_jmhType_B3 {
-}
-
